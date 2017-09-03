fb590cd5ec38c159604ca5fc5db3b0837fcb38f4
byWulf <wulf1337@gmail.com>
2017-09-03 02:08:27 +0200

    Threshold can now be set at the api call.

--- jigsawlutioner.js
+++ jigsawlutioner.js
@@ -3,8 +3,7 @@ const Jigsawlutioner = require('jigsawlutioner');
 exports.parseImage = (event, context, callback) => {
     let borderData = null;
     Jigsawlutioner.BorderFinder.findPieceBorder(Buffer.from(event.imageData, 'base64'), {
-        debug: true,
-        threshold: 225
+        threshold: event.threshold || 225
     }).then((borderResult) => {
         borderData = borderResult;
         return Jigsawlutioner.SideFinder.findSides(event.pieceIndex, borderData.path);
