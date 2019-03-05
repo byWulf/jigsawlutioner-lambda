e749a8f8571db0696e643e04093e4e02e25029c4
byWulf <wulf1337@gmail.com>
2019-03-05 19:50:24 +0100

    Fixed return of the transparent image

--- jigsawlutioner.js
+++ jigsawlutioner.js
@@ -1,23 +1,19 @@
 const Jigsawlutioner = require('jigsawlutioner');
 
-exports.parseImage = (event, context, callback) => {
-    let borderData = null;
-    Jigsawlutioner.BorderFinder.findPieceBorder(Buffer.from(event.imageData, 'base64'), {
-        threshold: event.threshold || 225,
-        reduction: event.reduction || 2,
-        returnTransparentImage: !!event.returnTransparentImage
-    }).then((borderResult) => {
-        borderData = borderResult;
-        return Jigsawlutioner.SideFinder.findSides(event.pieceIndex, borderData.path);
-    }).then((piece) => {
-        callback(null, {
-            pieceIndex: piece.pieceIndex,
-            sides: piece.sides,
-            boundingBox: borderData.boundingBox
+exports.parseImage = async (event, context, callback) => {
+    try {
+        let borderData = await Jigsawlutioner.BorderFinder.findPieceBorder(Buffer.from(event.imageData, 'base64'), {
+            threshold: event.threshold || 225,
+            reduction: event.reduction || 2,
+            returnTransparentImage: !!event.returnTransparentImage
         });
-    }).catch((err) => {
+
+        let sideData = await Jigsawlutioner.SideFinder.findSides(event.pieceIndex, borderData.path);
+
+        callback(null, Jigsawlutioner.PieceHelper.getLimitedPiece(borderData, sideData));
+    } catch (err) {
         callback(err);
-    });
+    }
 };
 
 exports.getPlacements = (event, context, callback) => {
