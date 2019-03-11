f1b717484382a2a9dc3c9db010bc2113ddc8bf80
byWulf <wulf1337@gmail.com>
2019-03-11 22:26:04 +0100

    Added logging

--- api.js
+++ api.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
+function getFormattedDate() {
+    let date = new Date();
+
+    let str = '';
+    str += (date.getHours() < 10 ? '0' : '') + date.getHours() + ':';
+    str += (date.getMinutes() < 10 ? '0' : '') + date.getMinutes() + ':';
+    str += (date.getSeconds() < 10 ? '0' : '') + date.getSeconds() + '.';
+    str += (date.getMilliseconds() < 100 ? '0' : '') + (date.getMilliseconds() < 10 ? '0' : '') + date.getMilliseconds();
+
+    return str;
+}
+
 // Loading config
 const configFile = __dirname + '/api_conf.json';
 if (!fs.existsSync(configFile)) {
@@ -23,6 +35,7 @@ if (!config.apiKey) {
 const app = express();
 app.use(bodyParser.json());
 
+let callCounter = 0;
 app.post('/api/v1/:endpoint', (request, response) => {
     const lambda = require('jigsawlutioner');
     const lambdaMappings = {
@@ -40,7 +53,10 @@ app.post('/api/v1/:endpoint', (request, response) => {
         return;
     }
 
+    callCounter++;
+    console.log('[' + getFormattedDate() + '] #' + callCounter + ' - Got request for endpoint "' + request.params.endpoint + '"');
     lambdaMappings[request.params.endpoint](request.body, null, (err, result) => {
+        console.log('[' + getFormattedDate() + '] #' + callCounter + ' - Sending response for endpoint "' + request.params.endpoint + '"');
         response.send(result);
     });
 });
